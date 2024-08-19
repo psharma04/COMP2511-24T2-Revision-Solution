1ac9872962466b36db66fe34fcf4808298daef4e
Pepsi <git@pshar.ma>
2024-08-19 21:27:58 +1000

    complete shipping

--- z5418973/app/src/main/java/unsw/shipping/DiscountDecorator.java
+++ z5418973/app/src/main/java/unsw/shipping/DiscountDecorator.java
@@ -1,7 +1,25 @@
 package unsw.shipping;
 
-public class DiscountDecorator {
+public class DiscountDecorator extends Product {
 
-    // TODO: Complete this class
+    private Product product;
+    private double discount;
 
+    public DiscountDecorator(Product product, double discount) {
+        this.product = product;
+        this.discount = (100 - discount)/100;
+    }
+
+    @Override
+    public double getPrice() {
+        return (product.getPrice() * discount);
+    }
+
+    public int getWeight() {
+        return product.getWeight();
+    }
+
+    public double getShippingCost() {
+        return product.getShippingCost();
+    }
 }
--- z5418973/app/src/main/java/unsw/shipping/FreeShippingDecorator.java
+++ z5418973/app/src/main/java/unsw/shipping/FreeShippingDecorator.java
@@ -1,7 +1,31 @@
 package unsw.shipping;
 
-public class FreeShippingDecorator {
+public class FreeShippingDecorator extends Product {
 
-    // TODO Complete this class
+    private Product product;
+    private int weight;
+    private int price;
+
+    public FreeShippingDecorator(Product product, int price, int weight) {
+        this.product = product;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return product.getWeight();
+    }
+
+    public double getPrice() {
+        return product.getPrice();
+    }
+
+    public double getShippingCost() {
+        if (product.getPrice() > price && product.getWeight() < weight) {
+            return 0;
+        }
+
+        return product.getShippingCost();
+    }
 
 }
